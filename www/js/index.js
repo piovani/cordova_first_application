c5769949486b88037cc776d135ac30eb3f76ee40
Allison Piovani <apiovani25@gmail.com>
2018-03-11 18:23:38 -0300

    Desenvolvimento inicial.

---
+++ www/js/index.js
@@ -0,0 +1,9 @@
+var informacoes = {
+    nome: 'Allison Piovani',
+    data_nascimento: '17/02/1992',
+    estado_civil: 'Solteiro',
+    email: 'apiovani25@gmail.com',
+    telefone: '(44)1234-1234',
+    endereco: 'Av. Brasil, 2000, Maringá',
+    foto: 'perfil.jpg'
+};
